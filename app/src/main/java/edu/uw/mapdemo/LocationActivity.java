c4af28b6f5aa876ba6ed2410947e0b10c00d1c21
luludust <elizabethduong96@gmail.com>
2016-02-02 13:37:45 -0800

    Class code

--- app/src/main/java/edu/uw/mapdemo/LocationActivity.java
+++ app/src/main/java/edu/uw/mapdemo/LocationActivity.java
@@ -13,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -32,22 +33,45 @@ import com.google.android.gms.location.LocationSettingsStatusCodes;
 import java.text.DateFormat;
 import java.util.Date;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private static final String TAG = "** LOC DEMO **";
+
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API) //use location
+                    .build();
+        }
     }
 
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
 
     /** Helper method for getting location **/
     public void getLocation(View v){
-        //TODO: Fill me in!
+        if(mGoogleApiClient != null) {
+            Location loc = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (loc != null) {
+                ((TextView) findViewById(R.id.txt_lat)).setText("" + loc.getLatitude());
+                ((TextView) findViewById(R.id.txt_lng)).setText("" + loc.getLongitude());
+            } else {
+                Log.v(TAG, "Last location is null");
+            }
+        }
     }
 
 
@@ -77,5 +101,35 @@ public class LocationActivity extends AppCompatActivity {
             default:
                 return super.onContextItemSelected(item);
         }
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        //when API has connected
+        getLocation(null);
+
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        ((TextView) findViewById(R.id.txt_lat)).setText("" + location.getLatitude());
+        ((TextView) findViewById(R.id.txt_lng)).setText("" + location.getLongitude());
     }
 }
